fa1d3e0cf2553e2b67face757c92f0f2e4dafc97
Todd Moscinski <toddmosc@microsoft.com>
2016-05-26 07:08:15 -0700

    Changing versions back to using -*'s.

--- src/dotnet/commands/dotnet-pack/Program.cs
+++ src/dotnet/commands/dotnet-pack/Program.cs
@@ -57,7 +57,7 @@ namespace Microsoft.DotNet.Tools.Compiler
                 var configValue = configuration.Value() ?? Constants.DefaultConfiguration;
                 var outputValue = output.Value();
                 var buildBasePathValue = PathUtility.GetFullPath(buildBasePath.Value());
-
+                var versionSuffixValue = versionSuffix.Value();
 
                 // Map dotnet pack arguments into NuGet pack arguments
                 string properties = $"Configuration={configValue}";
@@ -77,6 +77,11 @@ namespace Microsoft.DotNet.Tools.Compiler
                 {
                     arguments.Add("--base-path");
                     arguments.Add(buildBasePathValue);
+                }
+                if (!string.IsNullOrWhiteSpace(versionSuffixValue))
+                {
+                    arguments.Add("--suffix");
+                    arguments.Add(versionSuffixValue);
                 }
 				if (serviceable.HasValue()
 				{
